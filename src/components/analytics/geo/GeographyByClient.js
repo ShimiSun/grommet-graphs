abc72964a56f09b19801e563f58cec805d3607dc
shimisun <shimrit.yacobi@gmail.com>
2019-09-24 14:23:59 -0600

    changes animation behavior for Geo map

--- src/components/analytics/geo/GeographyByClient.js
+++ src/components/analytics/geo/GeographyByClient.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { Box, Diagram, Stack, WorldMap } from 'grommet';
 import { places, connections } from './data';
@@ -6,14 +6,25 @@ import { Tile } from '../../Tile';
 import { Legend } from '../legend/Legend';
 import { data as items } from '../legend/data';
 
-export const GeographyByClient = ({ ...rest }) => (
-  <Tile title="Geography by Client" {...rest}>
-    <Box flex overflow="hidden" alignSelf="center">
-      <Stack fill>
-        <WorldMap color="light-4" places={places} />
-        <Diagram connections={connections} />
-      </Stack>
-    </Box>
-    <Legend items={items} showValue={false} />
-  </Tile>
-);
+export const GeographyByClient = ({ ...rest }) => {
+  const [draw, setDraw] = useState();
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setDraw(!draw);
+    }, 3500);
+    return () => clearInterval(interval);
+  }, []);
+
+  return (
+    <Tile title="Geography by Client" {...rest}>
+      <Box flex overflow="hidden" alignSelf="center">
+        <Stack fill>
+          <WorldMap color="light-4" places={places} />
+          {draw && <Diagram connections={connections} />}
+        </Stack>
+      </Box>
+      <Legend items={items} showValue={false} />
+    </Tile>
+  );
+};
